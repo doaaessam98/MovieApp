8c9d62d0710ddf63cbbf36775ac2525a4ca4e45c
Doaa Essam <doaaessam2021@gmail.com>
2022-11-26 18:45:56 +0200

    set up navigation

--- app/src/main/java/com/example/movieapp/AppNavGraph.kt
+++ app/src/main/java/com/example/movieapp/AppNavGraph.kt
@@ -1,2 +1,28 @@
 package com.example.movieapp
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.movieapp.Screens.view.home.HomeScreen
+
+@Composable
+fun AppNav(modifier: Modifier,navController:NavHostController){
+
+  NavHost(navController = navController, startDestination =Screen.HomeScreen.route  ){
+      composable(Screen.HomeScreen.route){
+          HomeScreen(modifier = modifier)
+      }
+
+  }
+
+
+
+
+}
+ sealed class Screen(val route:String){
+     object HomeScreen:Screen(route = "home_screen")
+     object MovieDetailsScreen:Screen(route = "movie_details_screen")
+ }
--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +18,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+        AppNav(modifier = Modifier, navController = rememberNavController())
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
-    }
-}
+
